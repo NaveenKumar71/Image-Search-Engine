1f7ba6debd8f7ecdef906b5edf78d776549869ac
NaveenKumar71 <bknaveen712005@gmail.com>
2026-01-13 17:27:19 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const apiKey = "0MXcnTH9DGOg1_7zVh1oUrgJxG7dHAKcmYNoEQuNpS4";
+
+
+const searchBox = document.getElementById("search-box");
+const searchBtn = document.getElementById("search-btn");
+const showMoreBtn = document.getElementById("show-more");
+const searchResult = document.getElementById("search-result");
+const searchForm = document.getElementById("search-form");
+
+let keyword = "";
+let page = 1;
+
+async function show() {
+    keyword = searchBox.value;
+
+    const url = `https://api.unsplash.com/search/collections?page=${page}&query=${keyword}&client_id=${apiKey}&per_page=12`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+    const res = data.results;
+
+    if(page===1){
+        searchResult.innerHTML = "";
+    }
+
+    res.map((result) =>{
+        const img = document.createElement("img");
+        img.src = result.cover_photo.urls.small;
+        searchResult.appendChild(img);
+    })
+
+    showMoreBtn.style.display = "block"
+
+}
+
+searchForm.addEventListener("submit" ,(e) =>{
+    e.preventDefault();
+    page = 1;
+    show();
+})
+
+showMoreBtn.addEventListener(("click"),()=>{
+    page++;
+    show();
+})
+
